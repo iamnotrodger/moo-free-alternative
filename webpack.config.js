d57af31caa1bc3d7150bd989a82c758374e717df
iamnotrodger <rodger.retanal24@gmail.com>
2021-06-01 18:29:46 -0400

    [feature] Added Postcss to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -34,7 +34,12 @@ module.exports = {
 			},
 			{
 				test: /\.scss$/,
-				use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+				use: [
+					MiniCssExtractPlugin.loader,
+					'css-loader',
+					'postcss-loader',
+					'sass-loader',
+				],
 			},
 			{
 				test: /\.html$/,
@@ -42,4 +47,5 @@ module.exports = {
 			},
 		],
 	},
+	devtool: 'source-map',
 };
