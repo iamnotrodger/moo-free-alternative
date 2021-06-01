125aee2c7861fa9be9a1c1e9cd630f08466ebc97
iamnotrodger <rodger.retanal24@gmail.com>
2021-06-01 17:43:15 -0400

    [deploy] Added HTML to webpack compilation

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
@@ -7,12 +9,15 @@ module.exports = {
 			filename: 'style.css',
 			chunkFilename: '[name].css',
 		}),
+		new HtmlWebpackPlugin({
+			template: 'index.html',
+		}),
+		new CleanWebpackPlugin(),
 	],
 	entry: './script/index.js',
 	output: {
 		path: path.join(__dirname, 'dist'),
 		filename: 'bundle.js',
-		publicPath: '/dist',
 	},
 	module: {
 		rules: [
@@ -30,6 +35,10 @@ module.exports = {
 			{
 				test: /\.scss$/,
 				use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+			},
+			{
+				test: /\.html$/,
+				use: ['html-loader'],
 			},
 		],
 	},
