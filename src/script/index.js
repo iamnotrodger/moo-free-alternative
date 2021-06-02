9a46c115dbdec84172821f04f9cbe620dc2f2371
iamnotrodger <rodger.retanal24@gmail.com>
2021-06-02 02:00:34 -0400

    [feature] Added Clear filter button

--- src/script/index.js
+++ src/script/index.js
@@ -14,9 +14,14 @@ import { handleTabClick } from './tab';
 const checkoutTab = document.getElementById('checkout-tab');
 const checkoutButton = document.getElementById('checkout-button');
 const filter = document.getElementById('filter');
+const clearFilter = document.getElementById('filter-clear');
 const shopProducts = document.getElementById('shop-products');
 const shopTab = document.getElementById('shop-tab');
 const tabs = document.querySelectorAll('.tab__link');
+
+const handleClearFilter = () => {
+	filter.reset();
+};
 
 const handleFilterSubmit = (event) => {
 	event.preventDefault();
@@ -38,6 +43,7 @@ populateProducts(shopProducts, products);
 
 //Event listeners
 filter.addEventListener('submit', handleFilterSubmit);
+clearFilter.addEventListener('click', handleClearFilter);
 shopTab.addEventListener('click', handleShopTabClick);
 checkoutTab.addEventListener('click', handleCheckoutTabClick);
 checkoutButton.addEventListener('click', handleCheckout);
